a922178ff8d2b046cfe894a098806c271201f2e1
Perpy-del <perpetualmeninwa@gmail.com>
2026-04-12 16:57:40 +0100

    lint and prettier fix

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 export default {
   schema: './src/models/*.js',
-  out: './drizzle', 
+  out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DB_URL,
-  }
+  },
 };
